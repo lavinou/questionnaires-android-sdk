c589812b19f13808f350a49d58e6159e3fac544a
Vincent Turnier <vincent@lavinou.com>
2024-06-10 19:52:34 -0400

    bumped version

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.lavinou"
-version = project.findProperty("tag.version") ?: "0.0.0"
+version = project.findProperty("tag.version") ?: "0.1.13"
 
 android {
     namespace = "com.questionnaire.sdk"
